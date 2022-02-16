eacba4c2db846b9fadf4eb0f7a9f0ca4912cf263
KicassOS <szleper.pierre@gmail.com>
2022-02-17 00:36:11 +0100

    Default value for BUFFER_SIZE is 42

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,16 @@
 /*   By: pszleper <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 16:24:59 by pszleper          #+#    #+#             */
-/*   Updated: 2022/02/12 05:16:28 by pszleper         ###   ########.fr       */
+/*   Updated: 2022/02/17 00:09:59 by pszleper         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 # include <unistd.h>
 # include <stdlib.h>
