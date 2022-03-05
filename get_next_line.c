3c5d6c032fdbf7ab84162889d79e8d1822115266
KicassOS <szleper.pierre@gmail.com>
2022-03-05 15:19:24 +0100

    slight bugfix

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: pszleper <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 16:24:56 by pszleper          #+#    #+#             */
-/*   Updated: 2022/02/21 04:37:22 by pszleper         ###   ########.fr       */
+/*   Updated: 2022/03/05 15:18:45 by pszleper         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,12 +29,12 @@ char	*get_next_line(int fd)
 	while (bytes_read && ft_no_newline_found(line))
 	{
 		bytes_read = read(fd, line, BUFFER_SIZE);
-		if (bytes_read == -1)
+		if (bytes_read == -1 || bytes_read == 0)
 			return (NULL);
 		nl_index = ft_nl_index(line);
-		if (nl_index > 0)
+		if (nl_index >= 0)
 		{
-			if (ft_stash(stash, line, bytes_read - nl_index, nl_index))
+			if (!ft_stash(stash, line, bytes_read - nl_index, nl_index))
 				return (NULL);
 			return (line);
 		}
@@ -45,22 +45,23 @@ char	*get_next_line(int fd)
 char	ft_stash(char *stash, char *line, ssize_t stash_size, ssize_t nl_index)
 {
 	size_t	i;
-
+	printf("Executing ft_stash... ");
 	if (!stash)
 	{
 		stash = ft_calloc(stash_size, sizeof(char));
 		if (!stash)
 			return (0);
+		printf("ft_stash malloced successfully\n");
 	}
-	i = nl_index;
-	printf("------ BUFFER_SIZE: %d ---------\n", BUFFER_SIZE);
-	printf("nl_index: %ld\n", i);
-	while (line[i] && stash[i])
+	i = 0;
+	printf("Before loop: nl_index: %ld, line[%ld]: %c\n", i, i, line[i]);
+	while (i < nl_index && line[i])
 	{
 		stash[i] = line[i];
 		printf("line[i]: %c, stash[i]: %c, i: %ld\n", line[i], stash[i], i);
 		i++;
 	}
-	printf("stash: %s$\n", stash);
+	printf("stash: %s$^$\n", stash);
 	return (1);
 }
+
