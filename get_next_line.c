59e2f03ef38dc4b20835eb3ce8d61dbf3c4e54ca
KicassOS <szleper.pierre@gmail.com>
2022-03-16 05:44:22 +0100

    Removed redundant initialization for position and line initialzation to NULL which was causing a memory leak, never code when tired

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: pszleper <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 16:24:56 by pszleper          #+#    #+#             */
-/*   Updated: 2022/03/16 05:33:14 by pszleper         ###   ########.fr       */
+/*   Updated: 2022/03/16 05:40:57 by pszleper         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,7 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	line = NULL;
 	buffer = NULL;
-	position = 0;
 	position = ft_strchr_flag(line, '\n', 0);
 	while (position == -1 && position != -2)
 	{
